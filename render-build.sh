7f22383ba88404de266d1c88f0b2e05a48cc8152
Adınız SoyadınızCüneyt <cuneytdemir@hotmail.com>
2026-02-17 00:53:31 +0300

    pdf

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# 1. Python kütüphanelerini yükle
-pip install -r requirements.txt
-
-# 2. Sunucu paket listesini güncelle ve LibreOffice'i kur
-# Bu komut Render'ın Linux sunucusuna PDF dönüştürücü motoru yükler
-apt-get update && apt-get install -y libreoffice
